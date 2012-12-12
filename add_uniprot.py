a3600635471ddce133ede75bd869d3b109eeeeff
Joe Brown <brwnjm@gmail.com>
2012-12-12 14:23:33 -0700

    annotates a gene list

--- add_uniprot.py
+++ add_uniprot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
-add uniprot annotation for each entry.
+add uniprot annotation to gene list.
 """
 import sys
 from toolshed import reader, header
@@ -15,16 +15,16 @@ def todict(uniprot):
     return d, h
 
 def main(args):
-    db, uheader = todict(args.UNIPROT)
-    taheader = "chrom start stop name gname loc count".split()
-    for ta in reader(args.TA_ANNO, header=taheader):
+    db, uniprot_header = todict(args.UNIPROT)
+    header = ["genename"]
+    for gene in reader(args.GENELIST, header=header):
         # there is only one gene name per entry in the ta file
-        e = db.get(ta['gname'])
+        e = db.get(gene['genename'])
         fields = []
-        for h in taheader:
-            fields.append(ta[h])
+        for h in header:
+            fields.append(gene[h])
         if e:
-            for h in uheader:
+            for h in uniprot_header:
                 fields.append(e[h])
         print "\t".join(map(str, fields))
 
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     import argparse
     p = argparse.ArgumentParser(description=__doc__,
                     formatter_class=argparse.RawDescriptionHelpFormatter)
-    p.add_argument('TA_ANNO', help='result of annotate_ta.py')
+    p.add_argument('GENELIST', help='list of gene names')
     p.add_argument('UNIPROT', help='tab delimited uniprot db')
     args = p.parse_args()
     main(args)
